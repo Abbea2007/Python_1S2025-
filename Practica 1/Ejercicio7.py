4c8b80ff992e88a7808f26d3593dbfd4ee7eebae
Carlos Abea <carlosabea91@gmail.com>
2025-03-30 18:40:38 -0600

    Actualizacion del Ejercicio 7

--- Practica 1/Ejercicio7.py
+++ Practica 1/Ejercicio7.py
@@ -2,18 +2,32 @@
 en subcadenas utilizando un delimitador específico (por ejemplo, una coma 
 o un espacio). """
 
-list_cadenas = ["Hola mundo", "me llamo Carlos", "estudio ingeniería"]
-print(list_cadenas)
+list = []
 
-delimitador_especifico = " "
+def insertar_texto():
+   cadena_lista = input("Ingrese la cadena de texto que desea separar con un separador: ")
+   list.append(cadena_lista)
 
-def dividir_cadena(lista, caracter):
-    new_list = []
-    for cadena in lista: 
-        resultado = cadena.split(caracter) 
-        new_list.append(resultado)
-    return new_list
+delimitador_espeficio = input("Ingrese el delimitador especifico: ")
 
-result = dividir_cadena(list_cadenas, delimitador_especifico)
+def agregar_delimitador(lista, caracter):
+   new_list = []
+   for cadena in lista:
+      convertida = cadena.split(caracter)
+      new_list.append(convertida)
+   return new_list
 
-print(f"La cadena dividida por un delimitador especifico es: {result}")
+while True:
+    opcion = input("Desea ingresar otra cadena de texto: (s/n) => ").strip().lower()
+    if opcion == "s":
+       insertar_texto()
+       resultado = agregar_delimitador(list, delimitador_espeficio)
+       print(f"La cadena separada con un delimitador especifico es: {resultado}")
+    elif opcion == "n":
+       print("Gracias por usar")
+       break
+      
+
+
+
+
