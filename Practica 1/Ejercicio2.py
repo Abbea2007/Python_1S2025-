beebbcbfa424d43b776817d2d8be2f9ec60024ac
Carlos Abea <carlosabea91@gmail.com>
2025-03-30 20:18:30 -0600

    Mejore el ejercico 2

--- Practica 1/Ejercicio2.py
+++ Practica 1/Ejercicio2.py
@@ -1,17 +1,33 @@
 """ Escribe un programa que recorra una lista de cadenas y calcule la longitud 
 de cada cadena, almacenando el resultado en una nueva lista. """
 
-list = ["Hola mundo", "soy carlos abea", "estudio", "ingenieria en sistemas"]
-print(list)
+list = []
+
+def pedir_cadena():
+    cadenas = input("Ingrese la cadena: ")
+    subcadenas = cadenas.strip().split()
+    list.extend(subcadenas)  
+
+
 
 def calcular_longitud(lista):
     new_list = []
     for cadena in lista:
-        conteo = len(str(cadena))
-        new_list.append(conteo)
+        longitud = len(str(cadena))
+        new_list.append(longitud)
     return new_list
 
+pedir_cadena()
 resultado = calcular_longitud(list)
-
-print(f"La longitud de cada cadena es: {resultado}")
+print(f"Las longitudes de las cadenas son: {resultado}")
+while True:
+    list.clear()
+    option = input("¿Te gustaría ingresar otra cadena? (s/n) => ").strip().lower()
+    if option == "s":
+        pedir_cadena()
+        resultado = calcular_longitud(list)
+        print(f"Las longitudes de las cadenas son: {resultado}")
+    elif option == "n":
+        print("Gracias por usar")
+        break
     
