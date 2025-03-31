19cb0261de19c43862e247db15fbafa04b947107
Carlos Abea <carlosabea91@gmail.com>
2025-03-30 20:58:18 -0600

    Actualize el ejercicio 3

--- Practica 1/Ejercicio3.py
+++ Practica 1/Ejercicio3.py
@@ -3,19 +3,35 @@ cadena a mayúsculas o minúsculas dependiendo de un criterio. Si la
 longitud de la cadena es par, se convertirá a mayúsculas; si es impar, a 
 minúsculas."""
 
-list = ["Hola mundo", "soy carlos abea", "estudio", "ingenieria en sistemas"]
-print(list)
+list = []
 
-def cadena_mayuminu(lista):
-    new_list = []
-    for cadena in lista:
-        if len(cadena) % 2 == 0:
-            resultado = cadena.upper()
-        elif len(cadena) % 2 == 1:
-            resultado = cadena.lower()
-        new_list.append(resultado)
-    return new_list
+def pedir_cadena():
+    cadena = input("Ingrese la cadena: ").strip()
+    cadene_separada = cadena.split(",")
+    list.extend(cadene_separada)
 
-resultado_mayusmi = cadena_mayuminu(list)
-print(f"La nueva lista es: {resultado_mayusmi}")
+def convertir_cadena(lista):
+    for string in lista:
+        if len(string) % 2 == 0:
+         print(string.upper())
+        elif len(string) % 2 == 1:
+         print(string.lower())     
+    
+pedir_cadena()
+conversion = convertir_cadena(list)
+
+
+while True:
+    list.clear()
+    option = input("¿Deseas convertir otra cadena? (s/n) => ").strip().lower()
+    if option == "s":
+        pedir_cadena()
+        conversion = convertir_cadena(list)
+    elif option == "n":
+        print("Gracias por usar")
+        break
+
+
+
+
 
